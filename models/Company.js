c41163d3117b90c1a5cf3c73f2f494da51bd631c
goffu7 <135923234+goffu7@users.noreply.github.com>
2024-03-01 17:52:44 +0700

    hospital model to company model

--- models/Company.js
+++ models/Company.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const HospitalSchema = new mongoose.Schema({
+const CompanySchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please add a name'],
@@ -12,18 +12,15 @@ const HospitalSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add an address']
     },
-    district: {
+    website: {
         type: String,
-        required: [true, 'Please add a district']
+        required: [true, 'Please add a link to your website'],
+        match:["@^(http\:\/\/|https\:\/\/)?([a-z0-9][a-z0-9\-]*\.)+[a-z0-9][a-z0-9\-]*$@i",
+        "please add a valid link"]
     },
-    province: {
+    description: {
         type: String,
         required: [true, 'Please add a province']
-    },
-    postalcode: {
-        type: String,
-        required: [true, 'Please add a postal code'],
-        maxLength: [5, 'Postal Code can not be more than 5 digits']
     },
     tel: {
         type: String,
@@ -35,18 +32,18 @@ const HospitalSchema = new mongoose.Schema({
 });
 
 //Reverse populate with virtuals
-HospitalSchema.virtual('appointments', {
-    ref: 'Appointment',
+CompanySchema.virtual('interviews', {
+    ref: 'Interview',
     localField: '_id',
-    foreignField: 'hospital',
+    foreignField: 'company',
     justOne: false
 });
 
 //Cascade delete appointments when a hospital is deleted
-HospitalSchema.pre('deleteOne', { document: true, query: false }, async function(next) {
-    console.log(`Appointments being removed from hospital ${this._id}`);
-    await this.model('Appointment').deleteMany({hospital: this._id});
+CompanySchema.pre('deleteOne', { document: true, query: false }, async function(next) {
+    console.log(`Interview sessions being removed from Company ${this._id}`);
+    await this.model('Company').deleteMany({company: this._id});
     next();
 })
 
-module.exports = mongoose.model('Hospital', HospitalSchema);
+module.exports = mongoose.model('Company', CompanySchema);
